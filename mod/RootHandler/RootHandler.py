4c295f5127665b3287dedcc6db71abfcfb5f9bbd
Heyongdong <heyongdonghe@qq.com>
2015-10-22 17:01:34 +0800

    添加roothandler，对根目录的路由再分类

--- mod/RootHandler/RootHandler.py
+++ mod/RootHandler/RootHandler.py
@@ -16,7 +16,25 @@ class IndexPageHandler(tornado.web.RequestHandler):
         self.render('homepage/index.html',register=True,session=user_session)
 
 class HomePageHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        return self.application.db
+
     def get(self,args):
-        print "In home handler:"+args[0::]
+        askurl = args[0::]
+        print "In home handler:"+askurl
         user_session = str(self.get_secure_cookie("session"))
-        self.render('homepage/%s.html'%(args[0::]),register=True,session=user_session)
+        if askurl == "login":
+            ####login
+            if user_session == None:
+                self.render('homepage/login.html',register=True,session=user_session)
+            else:
+                self.render('homepage/login.html',register=True,session=user_session)
+        elif askurl == "index":
+            self.render('homepage/index.html',register=True,session=user_session)
+        else:
+            self.render('homepage/%s.html'%(askurl),register=True,session=user_session)
+
+
+
+
