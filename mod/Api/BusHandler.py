18c17f50288cfdd3b146247b3e8641b276c9751e
YongdongHe <heyongdonghe@qq.com>
2015-12-24 15:47:34 +0800

    fix：公交接口未实时获取lineID的bug

--- mod/Api/BusHandler.py
+++ mod/Api/BusHandler.py
@@ -1,5 +1,6 @@
 #coding=utf8
 from tornado.httpclient import HTTPRequest, AsyncHTTPClient,HTTPError
+from bs4 import BeautifulSoup
 import requests
 from sgmllib import SGMLParser
 import tornado.web
@@ -34,8 +35,15 @@ class BusHandler(tornado.web.RequestHandler):
     def get(self):
         try:
             client = AsyncHTTPClient()
-            urlToCompany = 'http://112.2.33.3:5902/wap/line.do?command=toDiss2&stationNo=2&stationName=%E5%87%BA%E5%8F%A3%E5%8A%A0%E5%B7%A5%E5%8C%BA&lineId=1726&inDown=1&lineName=711&strInfo=%E5%87%BA%E5%8F%A3%E5%8A%A0%E5%B7%A5%E5%8C%BA%E5%AE%A2%E8%BF%90%E7%AB%99%E2%86%92%E5%AE%89%E5%BE%B7%E9%97%A8'
-            urlToSchool = 'http://112.2.33.3:5902/wap/line.do?command=toDiss2&stationNo=19&stationName=%E5%B0%86%E5%86%9B%E5%A4%A7%E9%81%93%E8%BF%8E%E7%BF%A0%E8%B7%AF&lineId=1726&inDown=2&lineName=711&strInfo=%E5%AE%89%E5%BE%B7%E9%97%A8%E2%86%92%E5%87%BA%E5%8F%A3%E5%8A%A0%E5%B7%A5%E5%8C%BA%E5%AE%A2%E8%BF%90%E7%AB%99'
+            urlGetLineID = 'http://112.2.33.3:5902/wap/line.do?command=toLn'
+            reqLine = HTTPRequest(urlGetLineID,request_timeout=8)
+            resLineId = yield client.fetch(reqLine)
+            soup = BeautifulSoup(resLineId.body) 
+            lineID = soup.find('option',string='711')['value'][:-4]
+            print lineID
+            urlToCompany = 'http://112.2.33.3:5902/wap/line.do?command=toDiss2&stationNo=15&stationName=%E5%B0%86%E5%86%9B%E5%A4%A7%E9%81%93%E8%BF%8E%E7%BF%A0%E8%B7%AF&lineId='+lineID+'&inDown=1&lineName=711&strInfo=%E5%87%BA%E5%8F%A3%E5%8A%A0%E5%B7%A5%E5%8C%BA%E5%AE%A2%E8%BF%90%E7%AB%99%E2%86%92%E5%AE%89%E5%BE%B7%E9%97%A8'
+            print urlToCompany
+            urlToSchool = 'http://112.2.33.3:5902/wap/line.do?command=toDiss2&stationNo=19&stationName=%E5%B0%86%E5%86%9B%E5%A4%A7%E9%81%93%E8%BF%8E%E7%BF%A0%E8%B7%AF&lineId='+lineID+'&inDown=2&lineName=711&strInfo=%E5%AE%89%E5%BE%B7%E9%97%A8%E2%86%92%E5%87%BA%E5%8F%A3%E5%8A%A0%E5%B7%A5%E5%8C%BA%E5%AE%A2%E8%BF%90%E7%AB%99'
             reqtoC = HTTPRequest(urlToCompany,request_timeout=8)
             reqtoS = HTTPRequest(urlToSchool,request_timeout=8)
             restoC = yield client.fetch(reqtoC)
